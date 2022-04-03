7f9a4e0d9334ecae57b9d3b0999c6a5bd39810fa
radwaahmed20112000 <radwaahmed20112000@gmail.com>
2022-04-03 09:55:20 +0200

    Test File

---
+++ Back-end/src/main/java/hello.java
@@ -0,0 +1,8 @@
+import Services.PeakQueries.PeakUtilizationCpu;
+
+public class hello {
+    public static void main(String[] args) throws Exception {
+        PeakUtilizationCpu utilizationCpu = new PeakUtilizationCpu();
+        utilizationCpu.calculate("01-01-2023", "01-01-2023");
+    }
+}
