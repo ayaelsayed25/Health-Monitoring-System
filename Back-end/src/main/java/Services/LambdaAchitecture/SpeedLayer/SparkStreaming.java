a6cd95b7986eaf606d1a39ab67681b7e8a202b93
Sarah Osama <hylrcs@gmail.com>
2022-05-25 20:31:03 +0200

    Spark

--- Back-end/src/main/java/Services/LambdaAchitecture/SpeedLayer/SparkStreaming.java
+++ Back-end/src/main/java/Services/LambdaAchitecture/SpeedLayer/SparkStreaming.java
@@ -1,21 +1,42 @@
 package Services.LambdaAchitecture.SpeedLayer;
 
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-import org.apache.spark.SparkConf;
-import org.apache.spark.streaming.Durations;
-import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
-import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.streaming.StreamingQuery;
+import scala.Tuple2;
 
-public class SparkStreaming{
-    public static void main(String[] args) throws InterruptedException {
-        Logger.getLogger("org.apache").setLevel(Level.WARN);
-        SparkConf conf = new SparkConf().setAppName("sparkApp").setMaster("local[*]");
-        JavaStreamingContext jssc  = new JavaStreamingContext(conf, Durations.seconds(60));
-        JavaReceiverInputDStream<String> data = jssc.socketTextStream("localhost", 9999);
-        data.print();
-        jssc.start();
-        jssc.awaitTermination();
-        jssc.stop();
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.sql.SparkSession;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public final class SparkStreaming {
+    private static final Pattern SPACE = Pattern.compile(" ");
+
+    public static void main(String[] args) throws Exception {
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("JavaWordCount")
+                .master("local")
+                .getOrCreate();
+
+        JavaRDD<String> lines = spark.read().textFile("hello.txt").javaRDD();
+
+        JavaRDD<String> words = lines.flatMap(s -> Arrays.asList(SPACE.split(s)).iterator());
+
+        JavaPairRDD<String, Integer> ones = words.mapToPair(s -> new Tuple2<>(s, 1));
+
+        JavaPairRDD<String, Integer> counts = ones.reduceByKey((i1, i2) -> i1 + i2);
+
+        List<Tuple2<String, Integer>> output = counts.collect();
+        for (Tuple2<?,?> tuple : output) {
+            System.out.println(tuple._1() + ": " + tuple._2());
+        }
+        spark.stop();
     }
 }
