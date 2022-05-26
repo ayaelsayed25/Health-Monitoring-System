402071ca2b7ca4616b1310f0202ada9517e4b6cc
ayaelsayed25 <ayaelsayed52092@gmail.com>
2022-05-26 14:42:49 +0200

    Service queries

--- Spark/src/main/java/Spark.java
+++ Spark/src/main/java/Spark.java
@@ -1,68 +1,95 @@
-import org.apache.spark.api.java.function.FlatMapFunction;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Encoders;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.*;
 import org.apache.spark.sql.streaming.OutputMode;
-import org.apache.spark.sql.streaming.StreamingQuery;
-
-import java.util.Arrays;
+import org.apache.spark.sql.streaming.Trigger;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructType;
 import java.util.regex.Pattern;
+
+import static org.apache.spark.sql.functions.*;
 
 public final class Spark {
     private static final Pattern SPACE = Pattern.compile(" ");
 
     public static void main(String[] args) throws Exception {
+        //prepare spark session
         SparkSession spark = SparkSession
                 .builder()
-                .appName("JavaWordCount")
-                .master("local")
+                .appName("Speed Layer")
+                .master("local[*]")
+                .config("spark.driver.bindAddress", "127.0.0.1")
                 .getOrCreate();
 
-        Dataset<Row> lines = spark
-                .readStream()
-                .format("socket")
-                .option("host", "localhost")
-                .option("port", 65432)
-                .load();
+        //create json schema
+        StructType RAM = new StructType()
+                .add("Total", DataTypes.FloatType)
+                .add("Free", DataTypes.FloatType);
+        StructType Disk = new StructType()
+                .add("Total", DataTypes.FloatType)
+                .add("Free", DataTypes.FloatType);
 
-//// Split the lines into words
-//        Dataset<String> words = lines
-//                .as(Encoders.STRING())
-//                .flatMap((FlatMapFunction<String, String>) x -> Arrays.asList(x.split(" ")).iterator(), Encoders.STRING());
+        StructType schema = new StructType()
+                .add("serviceName", DataTypes.StringType)
+                .add("Timestamp", DataTypes.TimestampType)
+                .add("CPU", DataTypes.FloatType)
+                .add("RAM", RAM)
+                .add("Disk", Disk);
 
-// Generate running word count
-//        Dataset<Row> wordCounts = words.groupBy("value").count();
+        //read stream from json file
+        Dataset<Row> rawData = spark.readStream()
+                .format("json")
+                .schema(schema)
+                .json("/home/aya/Documents/GitHub/Health-Monitoring-System/streaming_dir");
 
-// Split the lines into words
-        Dataset<String> words = lines
-                .as(Encoders.STRING())
-                .flatMap((FlatMapFunction<String, String>) x -> Arrays.asList(x.split(" ")).iterator(), Encoders.STRING());
 
-// Generate running word count
-        Dataset<Row> wordCounts = words.groupBy("value").count();
+        //create realtime view
+        Dataset<Row> rawData2 = rawData.select(col("serviceName"), col("Timestamp"), col("CPU"),col("RAM.*"),
+                col("Disk.*"));
+        Dataset<Row> flattenedDS = rawData2.toDF("serviceName", "Timestamp", "CPU", "Total RAM","Free RAM","Total Disk","Free Disk");
+        final Column col_1 = functions.coalesce(flattenedDS.col("Total RAM"), functions.lit(0));
+        final Column col_2 = functions.coalesce(flattenedDS.col("Free RAM"), functions.lit(0));
+        final Column col_3 = functions.coalesce(flattenedDS.col("Total Disk"), functions.lit(0));
+        final Column col_4 = functions.coalesce(flattenedDS.col("Free Disk"), functions.lit(0));
+        Column ram_diff =  functions.abs(col_1.minus(col_2));
+        Column disk_diff =  functions.abs(col_3.minus(col_4));
+        Column utilization_ram = functions.abs(ram_diff.divide(col_1));
+        Column utilization_disk = functions.abs(disk_diff.divide(col_3));
+        flattenedDS = flattenedDS.withColumn("RAM Utilization", utilization_ram);
+        flattenedDS = flattenedDS.withColumn("Disk Utilization", utilization_disk);
+        flattenedDS = flattenedDS.drop("Total RAM", "Free RAM", "Total Disk", "Free Disk");
 
-        // Start running the query that prints the running counts to the console
-        StreamingQuery query = wordCounts.writeStream()
-                .outputMode("complete")
-                .format("console")
-                .start();
 
-//        lines.writeStream().format("parquet")
-//                .option("path","tryparquet")
-//                .option("checkpointLocation","checkpoint")
-//                .start().awaitTermination();
-//        spark.stop();
+        Dataset<Row> intervalsDS = flattenedDS
+                .withColumn("time", current_timestamp())
+                .withWatermark("time", "10 minutes")
+                .groupBy(window(flattenedDS.col("Timestamp"), "1 minutes"), flattenedDS.col("serviceName"))
+                .agg(count("*").alias("Message Count"),
+                        avg("CPU").alias("CPU Utilization Mean"),
+                        avg("RAM Utilization").alias("RAM Utilization Mean"),
+                        avg("Disk Utilization").alias("Disk Utilization Mean"),
+                        max("CPU").alias("CPU Utilization Peak"),
+                        max("RAM Utilization").alias("RAM Utilization Peak"),
+                        max("Disk Utilization").alias("Disk Utilization Peak"))
+                        .orderBy("window");
 
-//        StreamingQuery query = wordCounts
-//                .writeStream()
-//                .format("parquet")
-//                .option("path","tryparquet")
-//                .option("checkpointLocation","checkpoint")
-//                .outputMode(OutputMode.Append())
-//                .start();
-        query.awaitTermination();
-       spark.stop();
+
+
+
+        //write stream in parquet
+        intervalsDS.writeStream()
+        .format("parquet")
+                .option("path","/home/aya/parquets")
+                .option("checkpointLocation","/home/aya/checkpoints")
+                .trigger(Trigger.ProcessingTime("60 seconds"))
+                .outputMode(OutputMode.Append())
+//                .option("truncate", "false")
+                .start()
+                .awaitTermination();
+        spark.close();
+
+
+
+
 
     }
+
 }
