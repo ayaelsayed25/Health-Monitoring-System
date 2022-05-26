25aae40f353ba92ba2bb40da0f1cb461b0c8eece
Sarah Osama <hylrcs@gmail.com>
2022-05-26 08:26:21 +0200

    Spark Kafka configuration

--- Spark/src/main/java/Spark.java
+++ Spark/src/main/java/Spark.java
@@ -1,68 +1,95 @@
-import org.apache.spark.api.java.function.FlatMapFunction;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Encoders;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.streaming.OutputMode;
-import org.apache.spark.sql.streaming.StreamingQuery;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
 import java.util.regex.Pattern;
+
+import scala.Tuple2;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.streaming.api.java.*;
+import org.apache.spark.streaming.kafka010.ConsumerStrategies;
+import org.apache.spark.streaming.kafka010.KafkaUtils;
+import org.apache.spark.streaming.kafka010.LocationStrategies;
+import org.apache.spark.streaming.Durations;
+
+/**
+ * Consumes messages from one or more topics in Kafka and does wordcount.
+ * Usage: JavaDirectKafkaWordCount <brokers> <groupId> <topics>
+ *   <brokers> is a list of one or more Kafka brokers
+ *   <groupId> is a consumer group name to consume from topics
+ *   <topics> is a list of one or more kafka topics to consume from
+ *
+ * Example:
+ *    $ bin/run-example streaming.JavaDirectKafkaWordCount broker1-host:port,broker2-host:port \
+ *      consumer-group topic1,topic2
+ */
 
 public final class Spark {
     private static final Pattern SPACE = Pattern.compile(" ");
 
     public static void main(String[] args) throws Exception {
-        SparkSession spark = SparkSession
-                .builder()
-                .appName("JavaWordCount")
-                .master("local")
-                .getOrCreate();
+//        if (args.length < 3) {
+//            System.err.println("Usage: JavaDirectKafkaWordCount <brokers> <groupId> <topics>\n" +
+//                    "  <brokers> is a list of one or more Kafka brokers\n" +
+//                    "  <groupId> is a consumer group name to consume from topics\n" +
+//                    "  <topics> is a list of one or more kafka topics to consume from\n\n");
+//            System.exit(1);
+//        }
 
-        Dataset<Row> lines = spark
-                .readStream()
-                .format("socket")
-                .option("host", "localhost")
-                .option("port", 65432)
-                .load();
 
-//// Split the lines into words
-//        Dataset<String> words = lines
-//                .as(Encoders.STRING())
-//                .flatMap((FlatMapFunction<String, String>) x -> Arrays.asList(x.split(" ")).iterator(), Encoders.STRING());
+        String brokers =  "188.166.84.58:9092";
+        String groupId = "group1";
+        String topics = "topic";
 
-// Generate running word count
-//        Dataset<Row> wordCounts = words.groupBy("value").count();
+        // Create context with a 2 seconds batch interval
+        SparkConf sparkConf = new SparkConf().setAppName("JavaDirectKafkaWordCount").setMaster("local");
+        JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, Durations.seconds(2));
 
-// Split the lines into words
-        Dataset<String> words = lines
-                .as(Encoders.STRING())
-                .flatMap((FlatMapFunction<String, String>) x -> Arrays.asList(x.split(" ")).iterator(), Encoders.STRING());
+        Set<String> topicsSet = new HashSet<>(Arrays.asList(topics.split(",")));
+        Map<String, Object> kafkaParams = new HashMap<>();
+        kafkaParams.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers);
+        kafkaParams.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        kafkaParams.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        kafkaParams.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 
-// Generate running word count
-        Dataset<Row> wordCounts = words.groupBy("value").count();
+        // Create direct kafka stream with brokers and topics
+        JavaInputDStream<ConsumerRecord<String, String>> messages = KafkaUtils.createDirectStream(
+                jssc,
+                LocationStrategies.PreferConsistent(),
+                ConsumerStrategies.Subscribe(topicsSet, kafkaParams));
 
-        // Start running the query that prints the running counts to the console
-        StreamingQuery query = wordCounts.writeStream()
-                .outputMode("complete")
-                .format("console")
-                .start();
+        // Get the lines, split them into words, count the words and print
+        JavaDStream<String> lines = messages.map(ConsumerRecord::value);
+        JavaDStream<String> words = lines.flatMap(x -> Arrays.asList(SPACE.split(x)).iterator());
+        JavaPairDStream<String, Integer> wordCounts = words.mapToPair(s -> new Tuple2<>(s, 1))
+                .reduceByKey(Integer::sum);
+        wordCounts.print();
 
-//        lines.writeStream().format("parquet")
-//                .option("path","tryparquet")
-//                .option("checkpointLocation","checkpoint")
-//                .start().awaitTermination();
-//        spark.stop();
-
-//        StreamingQuery query = wordCounts
-//                .writeStream()
-//                .format("parquet")
-//                .option("path","tryparquet")
-//                .option("checkpointLocation","checkpoint")
-//                .outputMode(OutputMode.Append())
-//                .start();
-        query.awaitTermination();
-       spark.stop();
-
+        // Start the computation
+        jssc.start();
+        jssc.awaitTermination();
     }
 }
