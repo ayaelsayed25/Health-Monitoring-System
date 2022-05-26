b1c9fe5fb73ad3a6e45191603b349f25324a2414
Sarah Osama <hylrcs@gmail.com>
2022-05-26 18:14:57 +0200

    Writing to parquet files

--- Spark/src/main/java/Spark.java
+++ Spark/src/main/java/Spark.java
@@ -1,8 +1,10 @@
 import org.apache.spark.sql.*;
 import org.apache.spark.sql.streaming.OutputMode;
+import org.apache.spark.sql.streaming.StreamingQuery;
 import org.apache.spark.sql.streaming.Trigger;
 import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructType;
+
 import java.util.regex.Pattern;
 
 import static org.apache.spark.sql.functions.*;
@@ -38,11 +40,17 @@ public final class Spark {
         Dataset<Row> rawData = spark.readStream()
                 .format("json")
                 .schema(schema)
-                .json("/home/aya/Documents/GitHub/Health-Monitoring-System/streaming_dir");
+                .json("/home/sarah/Documents/streaming_dir");
 
+        StreamingQuery query = rawData.writeStream().queryName("stream")
+                .outputMode(OutputMode.Append())
+                .format("memory")
+                .trigger(Trigger.ProcessingTime(1_000))
+                .start();
 
+       Dataset<Row> data = spark.sql("SELECT * FROM stream");
         //create realtime view
-        Dataset<Row> rawData2 = rawData.select(col("serviceName"), col("Timestamp"), col("CPU"),col("RAM.*"),
+        Dataset<Row> rawData2 = data.select(col("serviceName"), col("Timestamp"), col("CPU"),col("RAM.*"),
                 col("Disk.*"));
         Dataset<Row> flattenedDS = rawData2.toDF("serviceName", "Timestamp", "CPU", "Total RAM","Free RAM","Total Disk","Free Disk");
         final Column col_1 = functions.coalesce(flattenedDS.col("Total RAM"), functions.lit(0));
@@ -62,29 +70,38 @@ public final class Spark {
                 .withColumn("time", current_timestamp())
                 .withWatermark("time", "10 minutes")
                 .groupBy(window(flattenedDS.col("Timestamp"), "1 minutes"), flattenedDS.col("serviceName"))
-                .agg(count("*").alias("Message Count"),
-                        avg("CPU").alias("CPU Utilization Mean"),
-                        avg("RAM Utilization").alias("RAM Utilization Mean"),
-                        avg("Disk Utilization").alias("Disk Utilization Mean"),
-                        max("CPU").alias("CPU Utilization Peak"),
-                        max("RAM Utilization").alias("RAM Utilization Peak"),
-                        max("Disk Utilization").alias("Disk Utilization Peak"))
-                        .orderBy("window");
+                .agg(count("*").alias("Message_Count"),
+                        avg("CPU").alias("CPU_Utilization_Mean"),
+                        avg("RAM Utilization").alias("RAM_Utilization_Mean"),
+                        avg("Disk Utilization").alias("Disk_Utilization_Mean"),
+                        max("CPU").alias("CPU_Utilization_Peak"),
+                        max("RAM Utilization").alias("RAM_Utilization_Peak"),
+                        max("Disk Utilization").alias("Disk_Utilization_Peak"))
+                .orderBy("window");
 
 
 
+        final String parquetFile = "test.parquet";
+        final String codec = "parquet";
+
+        intervalsDS.write().format(codec).mode(SaveMode.Append).save(parquetFile);
+        query.awaitTermination();
+
+//        intervalsDS.write().mode(SaveMode.Append).parquet("mama/hello.parquet");
 
         //write stream in parquet
-        intervalsDS.writeStream()
-        .format("parquet")
-                .option("path","/home/aya/parquets")
-                .option("checkpointLocation","/home/aya/checkpoints")
-                .trigger(Trigger.ProcessingTime("60 seconds"))
-                .outputMode(OutputMode.Append())
-//                .option("truncate", "false")
-                .start()
-                .awaitTermination();
-        spark.close();
+//        intervalsDS.writeStream()
+////                .format("parquet")
+////                .option("path","parquets")
+////                .option("checkpointLocation","checkpoints")
+//                .format("console")
+//                .outputMode("append")
+//                .trigger(ProcessingTime(1_000))
+////                .outputMode(OutputMode.Append())
+//////                .option("truncate", "false")
+//                .start()
+//                .awaitTermination();
+//        spark.close();
 
 
 
